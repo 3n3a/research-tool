be44620d5f41c03739b472672ed5b24b9b094e8a
3n3a <46775561+3n3a@users.noreply.github.com>
2023-12-22 11:33:13 +0100

    add dns.go
    add dns resolving via cloudflare dohttpy

---
+++ lib/dns.go
@@ -0,0 +1,54 @@
+package lib
+
+import (
+	"crypto/tls"
+	"fmt"
+
+	"github.com/go-resty/resty/v2"
+)
+
+type DNSQuestion struct {
+    Name string `json:"name"`
+    Type int `json:"type"`
+}
+
+type DNSAnswer struct {
+    Name string `json:"name"`
+    Type int `json:"type"`
+    TTL int
+    Data string `json:"data"`
+}
+
+type DNSRes struct {
+	Status int
+    TC bool
+    RD bool
+    RA bool
+    AD bool
+    CD bool
+    Question []DNSQuestion
+    Answer []DNSAnswer
+}
+
+func LookupDNSRecord(name string, type string) (DNSRes, error) {
+	client := resty.New()
+	client.SetTLSClientConfig(&tls.Config{ InsecureSkipVerify: true })
+	//client.SetDebug(true)
+
+	if domain == "" {
+		return DNSRes{}, nil
+	}
+	resp, err := client.R().
+		SetQueryParams(map[string]string{
+			"name": domain,
+            "type": type,
+		}).
+		SetHeader("Accept", "application/dns-json").
+		SetResult(&DNSRes{}).
+		Get("https://cloudflare-dns.com/dns-query")
+
+    // parse error codes
+    // https://developers.cloudflare.com/1.1.1.1/encryption/dns-over-https/make-api-requests/dns-json/
+
+	return resp, err
+}
