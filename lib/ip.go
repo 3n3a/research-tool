604c87a8d952cd710fe8163146adc3fa0b30db48
3n3a <46775561+3n3a@users.noreply.github.com>
2023-12-24 00:27:38 +0100

    Update ip.go

---
+++ lib/ip.go
@@ -0,0 +1,6 @@
+package lib
+
+// todo: add ip info
+
+// api here: http://ip-api.com/json/{query}?fields=status,message,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,offset,currency,isp,org,as,asname,reverse,mobile,proxy,hosting,query
+// docs: https://ip-api.com/docs/api:json
