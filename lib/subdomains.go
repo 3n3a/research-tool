2aa1ee76a13e06749d238c73a073c691b3454455
3n3a <info@3n3a.ch>
2023-12-25 19:12:26 +0000

    add generics to the game

--- lib/subdomains.go
+++ lib/subdomains.go
@@ -4,20 +4,70 @@ import (
 	"fmt"
 )
 
-type SubdomainRes []string
-type Subdomains struct {
+type SubdomainResARPSyndicate []string
+
+type SubdomainCRTSH struct {
+	IssueCaId int `json:"issuer_ca_id"`
+	IssuerName string `json:"issuer_name"`
+	CommonName string `json:"common_name"`
+	NameValue string `json:"name_value"`
+	Id int `json:"id"`
+	EntryTimestamp string `json:"entry_timestamp"`
+	NotBefore string `json:"not_before"`
+	NotAfter string `json:"not_after"`
+	SerialNumber string `json:"serial_number"`
+	ResultCount int `json:"result_count"`
+}
+type SubdomainResCRTSH []SubdomainCRTSH
+
+type SubdomainTypes interface {
+	SubdomainResARPSyndicate | SubdomainResCRTSH
+}
+type Subdomains[T SubdomainTypes] struct {
 	Domain string
-	List SubdomainRes
+	List T // TOdo: make generic
 }
 
-func GetSubdomains(domain string) (Subdomains, error) {
+func GetSubdomainsCRTSH(domain string) (Subdomains[SubdomainResCRTSH], error) {
+	// https://crt.sh/?q=%.3n3a.ch&output=json&exclude=expired
+
 	client := NewHTTPClient()
 
-	subdomainRes := SubdomainRes{} 
+	subdomainRes := SubdomainResCRTSH{} 
 	
 	if domain == "" {
-		return Subdomains{
-			List: make(SubdomainRes, 0),
+		return Subdomains[SubdomainResCRTSH]{
+			List: make(SubdomainResCRTSH, 0),
+			Domain: domain,
+		}, nil
+	}
+	_, err := client.R().
+		SetQueryParams(map[string]string{
+			"domain": domain,
+		}).
+		SetHeader("Accept", "application/json").
+		SetResult(&subdomainRes).
+		Get("https://api.subdomain.center")
+
+	for i, sub := range subdomainRes {
+		// todo: filter name value for only usable and unique ones
+		subdomainRes[i] = fmt.Sprintf("http://%s", sub)
+	}
+
+	return Subdomains[SubdomainResCRTSH]{
+		List: subdomainRes,
+		Domain: domain,
+	}, err
+}
+
+func GetSubdomainsARPSyndicate(domain string) (Subdomains[SubdomainResARPSyndicate], error) {
+	client := NewHTTPClient()
+
+	subdomainRes := SubdomainResARPSyndicate{} 
+	
+	if domain == "" {
+		return Subdomains[SubdomainResARPSyndicate]{
+			List: make(SubdomainResARPSyndicate, 0),
 			Domain: domain,
 		}, nil
 	}
@@ -33,7 +83,7 @@ func GetSubdomains(domain string) (Subdomains, error) {
 		subdomainRes[i] = fmt.Sprintf("http://%s", sub)
 	}
 
-	return Subdomains{
+	return Subdomains[SubdomainResARPSyndicate]{
 		List: subdomainRes,
 		Domain: domain,
 	}, err
