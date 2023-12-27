61cb4e76b273f93466ba1bcc4084f682f2596d84
3n3a <info@3n3a.ch>
2023-12-27 20:02:00 +0000

    feature ANY dns query possible + more dns types

--- lib/dns/dns.go
+++ lib/dns/dns.go
@@ -1,6 +1,8 @@
 package dns
 
-import "github.com/3n3a/research-tool/lib/utils"
+import (
+	"github.com/3n3a/research-tool/lib/utils"
+)
 
 type DNSQuestion struct {
     Name string `json:"name"`
@@ -10,8 +12,13 @@ type DNSQuestion struct {
 type DNSAnswer struct {
     Name string `json:"name"`
     Type int `json:"type"`
+	HumanType string `json:"human_type"`
     TTL int
     Data string `json:"data"`
+}
+
+func (d *DNSAnswer) TransformType() {
+	d.HumanType = GetResourceRecordType(d.Type)
 }
 
 type DNSRes struct {
@@ -28,34 +35,37 @@ type DNSRes struct {
     Answer []DNSAnswer
 }
 
-func GetDNSTypes() []string {
-	DNS_TYPE_LIST := []string{
-		"A",
-		"AAAA",
-		"ANY",
-		"CAA",
-		"CNAME",
-		"DNSKEY",
-		"DS",
-		"MX",
-		"NS",
-		"PTR",
-		"SOA",
-		"SRV",
-		"TLSA",
-		"TSIG",
-		"TXT",
+func LookupAnyDNSRecord(name string) (DNSRes, error) {
+	var res DNSRes
+	var err error
+	for _, dnstype := range GetDNSTypes() {
+		if dnstype != "ANY" {
+			tempRes, tempErr := LookupDNSRecord(name, dnstype)
+			res.Answer = append(res.Answer, tempRes.Answer...)
+			err = tempErr
+		}
 	}
 
-	return DNS_TYPE_LIST
+	res.DNSDomain = name
+	res.DNSType = "ANY"
+
+	return res, err
 }
 
 func LookupDNSRecord(name string, dnstype string) (DNSRes, error) {
-	client := utils.NewHTTPClient()
-
-	if name == "" {
+	// Handle Empty Input Name
+	if name == "" || dnstype == "" {
 		return DNSRes{}, nil
 	}
+
+	// Any
+	if dnstype == "ANY" {
+		return LookupAnyDNSRecord(name)
+	}
+
+	// All other than ANY
+	client := utils.NewHTTPClient()
+
 	resp, err := client.R().
 		SetQueryParams(map[string]string{
     		"name": name,
@@ -71,6 +81,13 @@ func LookupDNSRecord(name string, dnstype string) (DNSRes, error) {
 	dnsres := *(resp.Result().(*DNSRes))
 	dnsres.DNSDomain = name
 	dnsres.DNSType = dnstype
+
+	// process dns answers array
+	for i, answer := range dnsres.Answer {
+		answer.TransformType()
+		dnsres.Answer[i] = answer
+	}
+
 
 	return dnsres, err
 }
