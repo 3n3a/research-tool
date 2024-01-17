d785f99dbebade3050cc1386c3ce64c3c9ec12bc
3n3a <46775561+3n3a@users.noreply.github.com>
2024-01-17 08:04:30 +0100

    Update ip.go

--- lib/ip/ip.go
+++ lib/ip/ip.go
@@ -2,5 +2,5 @@ package ip
 
 // todo: add ip info
 
-// api here: http://ip-api.com/json/{query}?fields=status,message,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,offset,currency,isp,org,as,asname,reverse,mobile,proxy,hosting,query
+// api here: http://ip-api.com/json/{query}?fields=66846719
 // docs: https://ip-api.com/docs/api:json
