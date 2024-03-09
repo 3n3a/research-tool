654e92aaa09c18e5d30c9674aa5364f2398469b9
3n3a <46775561+3n3a@users.noreply.github.com>
2024-03-09 21:25:05 +0100

    Update ip.go

--- handlers/ip.go
+++ handlers/ip.go
@@ -7,6 +7,10 @@ import (
 	ip "github.com/3n3a/research-tool/lib/ip"
 )
 
+const (
+ CLIENT_IP_HEADER = "CF-Connecting-IP"
+)
+
 func SetupIP() {
 	pageInfo.AddMenuEntry("IP Lookup", "/ip-address", 10)
 	app.Get("/ip-address", IPLookup)
@@ -16,6 +20,12 @@ func SetupIP() {
 func IPLookup(c *fiber.Ctx) error {
 	ipaddr := c.Query("ipaddr")
 	additionalInfo := common.IPPage{}
+
+ // TODO: IP / Domain validation
+ if len(ipaddr) < 4 {
+  // set to users ip
+  ipaddr = c.Get(CLIENT_IP_HEADER)
+ }
 
 	ipres, err := ip.LookupIPInfo(ipaddr)
 	if err != nil {
