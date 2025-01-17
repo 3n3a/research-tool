d5ecd829cd50212a8e620c6624cc56a7f3dc8c71
3n3a <46775561+3n3a@users.noreply.github.com>
2025-01-17 07:25:20 +0100

    dynamic button and primeng support for more fields

--- src/app/components/dynamic-form-question/dynamic-form-question.component.ts
+++ src/app/components/dynamic-form-question/dynamic-form-question.component.ts
@@ -2,16 +2,18 @@ import { Component, Input } from '@angular/core';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { QuestionBase } from '../../types/question-base';
 
+import { InputTextModule } from 'primeng/inputtext';
+
 @Component({
   selector: 'comp-dynamic-form-question',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, InputTextModule],
   templateUrl: './dynamic-form-question.component.html',
   styleUrl: './dynamic-form-question.component.scss'
 })
 export class DynamicFormQuestionComponent {
   @Input() question!: QuestionBase<string>;
   @Input() form!: FormGroup;
-  
+
   get isValid() {
     return this.form.controls[this.question.key].valid;
   }
--- src/app/components/dynamic-form/dynamic-form.component.ts
+++ src/app/components/dynamic-form/dynamic-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit, input } from '@angular/core';
 import { DynamicFormQuestionComponent } from '../dynamic-form-question/dynamic-form-question.component';
 import {FormGroup, ReactiveFormsModule} from '@angular/forms';
 import { QuestionBase } from '../../types/question-base';
@@ -19,14 +19,15 @@ import { ButtonModule } from 'primeng/button';
   styleUrl: './dynamic-form.component.scss'
 })
 export class DynamicFormComponent implements OnInit {
-  @Input() questions: QuestionBase<string>[] | null = [];
+  readonly questions = input<QuestionBase<string>[] | null>([]);
+  readonly submitButtonText = input<string>("Submit");
   form!: FormGroup;
   payLoad = '';
 
   constructor(private qcs: DynamicFormService) {}
 
   ngOnInit() {
-    this.form = this.qcs.toFormGroup(this.questions as QuestionBase<string>[]);
+    this.form = this.qcs.toFormGroup(this.questions() as QuestionBase<string>[]);
   }
 
   onSubmit() {
