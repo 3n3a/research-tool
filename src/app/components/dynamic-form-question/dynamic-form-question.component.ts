6767ccc65e4a013e6c07926ed7f9328f8fceb09a
3n3a <46775561+3n3a@users.noreply.github.com>
2025-01-23 17:00:40 +0100

    updates

--- src/app/components/dynamic-form-question/dynamic-form-question.component.ts
+++ src/app/components/dynamic-form-question/dynamic-form-question.component.ts
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { Component, Input } from '@angular/core';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { QuestionBase } from '../../types/question-base';
@@ -19,3 +20,25 @@ export class DynamicFormQuestionComponent {
     return this.form.controls[this.question.key].valid;
   }
 }
+=======
+import { Component, Input } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { QuestionBase } from '../../types/question-base';
+
+import { InputTextModule } from 'primeng/inputtext';
+
+@Component({
+  selector: 'comp-dynamic-form-question',
+  imports: [ReactiveFormsModule, InputTextModule],
+  templateUrl: './dynamic-form-question.component.html',
+  styleUrl: './dynamic-form-question.component.scss'
+})
+export class DynamicFormQuestionComponent {
+  @Input() question!: QuestionBase<string>;
+  @Input() form!: FormGroup;
+
+  get isValid() {
+    return this.form.controls[this.question.key].valid;
+  }
+}
+>>>>>>> Stashed changes
