776e9f9b6d3ac1799d3c0e2c0adab3fca13c983a
3n3a <46775561+3n3a@users.noreply.github.com>
2025-02-01 16:48:24 +0100

    fix merge conflict

--- src/app/components/dynamic-form-question/dynamic-form-question.component.ts
+++ src/app/components/dynamic-form-question/dynamic-form-question.component.ts
@@ -1,26 +1,3 @@
-<<<<<<< Updated upstream
-import { Component, Input } from '@angular/core';
-import { FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { QuestionBase } from '../../types/question-base';
-
-import { InputTextModule } from 'primeng/inputtext';
-import { SelectModule } from 'primeng/select';
-
-@Component({
-  selector: 'comp-dynamic-form-question',
-  imports: [ReactiveFormsModule, InputTextModule, SelectModule],
-  templateUrl: './dynamic-form-question.component.html',
-  styleUrl: './dynamic-form-question.component.scss'
-})
-export class DynamicFormQuestionComponent {
-  @Input() question!: QuestionBase<string>;
-  @Input() form!: FormGroup;
-
-  get isValid() {
-    return this.form.controls[this.question.key].valid;
-  }
-}
-=======
 import { Component, Input } from '@angular/core';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { QuestionBase } from '../../types/question-base';
@@ -41,4 +18,3 @@ export class DynamicFormQuestionComponent {
     return this.form.controls[this.question.key].valid;
   }
 }
->>>>>>> Stashed changes
